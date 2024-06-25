9cc8279701413f75e4c0fa03565f44cfabe32b4e
Philip Zimmermann <pzi@ito-consult.de>
2024-06-25 21:34:17 +0200

    add components with daisyUi

--- components/Header.vue
+++ components/Header.vue
@@ -1,57 +1,24 @@
 <template>
-  <div class="bg-white relative">
-    <header class="p-4 md:p-16 relative">
-      <div class="container mx-auto flex justify-between items-center">
-        <h1 class="text-2xl md:text-3xl font-bold">Gartenfuchs</h1>
-      </div>
-      <!-- Wrapper for the black and green divs on desktops -->
-      <div
-        class="hidden md:flex absolute -bottom-8 z-10 left-0 right-0 justify-between container mx-auto"
-      >
-        <!-- black navigation -->
-        <div class="w-4/5 flex items-center bg-dark p-5">
-          <nav>
-            <ul class="flex space-x-4 text-white">
-              <li><NuxtLink to="/">Home</NuxtLink></li>
-              <li><NuxtLink to="/impressum">Über uns</NuxtLink></li>
-              <li><NuxtLink to="/kontakt">Dienstleistungen</NuxtLink></li>
-              <li><NuxtLink to="/datenschutz">Bilder</NuxtLink></li>
-              <li><NuxtLink to="/projekte">Kontakt</NuxtLink></li>
-            </ul>
-          </nav>
-        </div>
-        <!-- green div -->
-        <div class="w-1/5 p-5 bg-gf_green flex items-center gap-2 text-white">
-          <a href="tel:05341 54545454" class="flex items-center space-x-2">
-            <SvgIcon iconType="call" viewBox="0 0 100 100" />
-            <span>05341 / 347 384 4</span>
-          </a>
-        </div>
-      </div>
-      <!-- Wrapper for the black and green divs on mobile -->
-      <div class="md:hidden flex flex-col items-center mt-4">
-        <!-- green div -->
-        <div
-          class="w-full p-3 bg-gf_green flex items-center justify-center text-white"
+  <div class="container navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">Gartenfuchs</a>
+    </div>
+    <div class="flex-none">
+      <button class="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="inline-block h-5 w-5 stroke-current"
         >
-          <a href="tel:05341 54545454" class="flex items-center space-x-2">
-            <SvgIcon iconType="call" viewBox="0 0 100 100" />
-            <span>05341 / 347 384 4</span>
-          </a>
-        </div>
-        <!-- black navigation -->
-        <div class="w-full bg-dark p-5 mt-4">
-          <nav>
-            <ul class="flex flex-col items-center space-y-4 text-white">
-              <li><NuxtLink to="/">Home</NuxtLink></li>
-              <li><NuxtLink to="/impressum">Über uns</NuxtLink></li>
-              <li><NuxtLink to="/kontakt">Dienstleistungen</NuxtLink></li>
-              <li><NuxtLink to="/datenschutz">Bilder</NuxtLink></li>
-              <li><NuxtLink to="/projekte">Kontakt</NuxtLink></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+          ></path>
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,47 @@
 <template>
   <div class="overflow-hidden mx-auto max-w-[1800px]">
-    <div class="relative banner">
-      <img
-        src="https://picsum.photos/id/28/1800/500"
-        alt="Banner"
-        class="md:h-[500px] w-full object-cover"
-      />
-      <div class="absolute inset-0 bg-black opacity-25"></div>
-      <div
-        class="absolute z-10 inset-0 flex items-center justify-center text-white px-4"
-      >
-        <div class="text-center">
-          <span
-            class="block text-xl text-title_blue-500 sm:text-3xl md:text-4xl font-normal drop-shadow-2xl"
-            >Willkommen beim Gartenfuchs</span
-          >
-          <span
-            class="block text-2xl text-title_blue-500 sm:text-4xl md:text-5xl font-black drop-shadow-2xl"
-            >Wir gestalten Ihren Garten</span
-          >
-          <div class="mt-10 space-x-10">
-            <NuxtLink
-              to="/kontakt"
-              class="inline-block px-4 py-2 bg-gf_green text-white rounded-md shadow-md"
-              >Jetzt Kontakt aufnehmen</NuxtLink
-            >
-            <NuxtLink
-              to="/projekte"
-              class="inline-block px-4 py-2 bg-white text-dark rounded-md shadow-md"
-              >Unsere Projekte ansehen</NuxtLink
-            >
-          </div>
+    <div
+      class="hero md:h-[500px] w-full object-cover"
+      style="background-image: url(https://picsum.photos/id/28/1800/500)"
+    >
+      <div class="hero-overlay bg-opacity-60"></div>
+      <div class="hero-content text-neutral-content text-center">
+        <div class="max-w-md">
+          <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+          <p class="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button class="btn btn-primary text-accent">Get Started</button>
         </div>
       </div>
     </div>
+  </div>
+  <div class="container flex w-full flex-col lg:flex-row mt-16 text-secondary">
+    <div class="w-1/2">
+      <h1>
+        Willkommen bei <span class="text-primary">Gartenfuchs</span> – Ihr
+        Experte für Gartenpflege in Braunschweig
+      </h1>
+      <p class="mt-16 flex flex-col gap-16">
+        <span>
+          Seit vier Jahren steht Gartenfuchs für erstklassige Gartenpflege und
+          Landschaftsgestaltung in Braunschweig. Wir sind stolz darauf, Ihre
+          erste Wahl für alle Ihre Gartenbedürfnisse zu sein. Unser engagiertes
+          Team von Fachleuten verwandelt Ihre Grünflächen in Oasen der Ruhe und
+          Schönheit. </span
+        ><span>
+          Ob regelmäßige Gartenpflege, Neugestaltung oder spezielle Projekte –
+          bei uns sind Sie in den besten Händen. Vertrauen Sie auf unsere
+          Erfahrung und Leidenschaft für Gartenarbeit und lassen Sie uns
+          gemeinsam Ihre Gartenträume verwirklichen. Willkommen bei Gartenfuchs
+          – wo Ihr Garten in besten Händen ist.
+        </span>
+      </p>
+    </div>
+
+    <div class="w-1/2">content</div>
   </div>
 </template>
 
